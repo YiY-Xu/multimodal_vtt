1095ed2eca90a8b8d0db9bd50fe9b2d09ec21a40
Yi <yx2@andrew.cmu.edu>
2019-03-27 13:20:39 -0400

    fix a budg

--- train_vtt.py
+++ train_vtt.py
@@ -197,7 +197,6 @@ def validate(opt, val_loader, model):
 
     # Yi: add for recall metrics
     r, rt = i2t(img_embs, cap_embs, img_embs, cap_embs, measure=opt.measure, return_ranks=True)
-    ri, rti = t2i(img_embs1, cap_embs1, img_embs1, cap_embs1, measure=opt.measure, return_ranks=True)
     ar = (r[0] + r[1] + r[2]) / 3
     print("Average i2t Recall: %.1f" % ar)
     print("Image to text: %.1f %.1f %.1f %.1f %.1f" % r)
